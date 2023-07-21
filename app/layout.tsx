5fba120a97ab74c6491b487f87d89e6ad92d0177
jonny desktop <jon@uslms.com>
2023-07-21 15:08:03 -0600

    added the signup and sign in modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import "./globals.css";
 import type { Metadata } from "next";
+import React, { useState } from "react";
 import { Inter } from "next/font/google";
 import Navbar from "./(components)/navbar";
 import Footer from "./(components)/footer";
+import SignInModal from "./user/signin/page";
+import SignUpModal from "./user/signup/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +21,32 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [isSignInModalOpen, setIsSignInModalOpen] = useState<boolean>(false);
+  const [isSignUpModalOpen, setIsSignUpModalOpen] = useState<boolean>(false);
+
+  // const handleOpenSignInModal = () => {
+  //   setIsSignInModalOpen(true);
+  // };
+
+  // const handleCloseSignInModal = () => {
+  //   setIsSignInModalOpen(false);
+  // };
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
+        <Navbar
+          setIsSignInModalOpen={setIsSignInModalOpen}
+          setIsSignUpModalOpen={setIsSignUpModalOpen}
+        />
         {children}
+        <h1>HOME</h1>
         <Footer></Footer>
+        {isSignUpModalOpen && (
+          <SignUpModal setIsSignUpModalOpen={setIsSignUpModalOpen} />
+        )}
+        {isSignInModalOpen && (
+          <SignInModal setIsSignInModalOpen={setIsSignInModalOpen} />
+        )}
       </body>
     </html>
   );
