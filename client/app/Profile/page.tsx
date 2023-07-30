bc59475b370a3e0cd45ff7f30beb123a97b1777d
Carter <carreejoh4637@gmail.com>
2023-07-30 16:46:35 -0600

    Profile section started, some styles, pushing for no merge conflicts

---
+++ client/app/Profile/page.tsx
@@ -0,0 +1,16 @@
+import "../(styles)/profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profileMainDiv">
+      <div className="grid grid-cols-10 gap-4">
+        <div className="col-span-2">
+            {/* For Spacing */}
+        </div>
+        <div className="col-span-2 bg-slate-400 w-[100%] h-5">
+            
+        </div>
+      </div>
+    </div>
+  );
+}
