187ff8a40961ccafdab71c52e90aec0900de2cbf
Carter <carreejoh4637@gmail.com>
2023-07-31 17:47:56 -0600

    Search bar and Explore links different now

--- client/app/(components)/toggleSidebar.tsx
+++ client/app/(components)/toggleSidebar.tsx
@@ -68,21 +68,17 @@ const ToggleSidebar = ({ props, type, sidebarOpacity }: toggle) => {
     if (type === "Search") {
       return (
         <div
-          className={`top-0 left-0 w-48 text-white fixed h-full z-10 ease-in-out duration-300 toggleSidebarMainDiv ${
+          className={`top-32 left-5 w-72 text-white fixed h-full z-10 ease-in-out duration-300 toggleSidebarMainDiv ${
             sidebarOpacity
               ? "w-0 opacity-0 translate-x-0 "
               : "w-100 opacity-1 translate-x-20"
           }`}
         >
-          <div className="ml-20 mt-[50px]">
-            {/* Idk why this shit isnt working */}
-
             <input
               type="text"
               placeholder="Search Crusaders..."
-              className="relative px-4 py-2 h-8 rounded-lg bg-white z-60"
+              className="text-white mt-2 rounded-xl p-1 bg-coolGray border-solid border-customPurple border-t-2 border-r-2 border-l-2 border-b-2 outline-none w-64"
             />
-          </div>
         </div>
       );
     }
