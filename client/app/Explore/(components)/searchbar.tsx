fa8b6b4b2130adc9147cd2c6c829075ca0cb437a
Carter <carreejoh4637@gmail.com>
2023-07-31 20:18:30 -0600

    Fixed Merges issues

--- client/app/Explore/(components)/searchbar.tsx
+++ client/app/Explore/(components)/searchbar.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-
 import { useState } from "react";
 
 // Remember to import words or whatever you're using to store all the words the user can search for
 
 export default function Searchbar() {
-    const [result1, setResult1] = useState("")
-    const [result2, setResult2] = useState("")
-    const [result3, setResult3] = useState("")
-    const [result4, setResult4] = useState("")
-    const [result5, setResult5] = useState("")
+  const [result1, setResult1] = useState("");
+  const [result2, setResult2] = useState("");
+  const [result3, setResult3] = useState("");
+  const [result4, setResult4] = useState("");
+  const [result5, setResult5] = useState("");
   return (
     <form>
       <div className="flex">
@@ -18,6 +17,14 @@ export default function Searchbar() {
           <input
             type="search"
             id="search-dropdown"
+            className="rounded-lg block p-2.5 w-full z-20 text-sm text-white bg-transparent rounded-r-lg border-l-customPurple border-l-2 border border-customPurple focus:ring-transparent focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+            placeholder="Any Hashtag..."
+            required
+          />
+
+          <button
+            type="submit"
+            className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-customPurple rounded-r-lg border border-customPurple hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-customPurple dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <svg
               className="w-4 h-4"
@@ -36,6 +43,7 @@ export default function Searchbar() {
             </svg>
             <span className="sr-only">Search</span>
           </button>
+
           <ul
             className="absolute z-[1000] float-left m-0 w-full mt-1 list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
             aria-labelledby="dropdownMenuButton1"
@@ -78,7 +86,6 @@ export default function Searchbar() {
               </a>
             </li>
           </ul>
-
         </div>
       </div>
     </form>
