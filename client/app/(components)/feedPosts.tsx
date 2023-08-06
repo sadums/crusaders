a8a6cc70a40d190cf3913859364dc5d9bb750e28
Carter <carreejoh4637@gmail.com>
2023-08-05 19:25:00 -0600

    Comments section animation fixed, styling to feed post modal

--- client/app/(components)/feedPosts.tsx
+++ client/app/(components)/feedPosts.tsx
@@ -25,7 +25,6 @@ interface FeedPostsProps {
 }
 
 function FeedPosts({ posts, postClickHandler }: FeedPostsProps) {
-
   function formatDate(timestamp: string) {
     let date = new Date(parseInt(timestamp));
 
@@ -37,9 +36,7 @@ function FeedPosts({ posts, postClickHandler }: FeedPostsProps) {
     let formattedMinute = minute < 10 ? `0${minute}` : `${minute}`;
 
     return `${month}/${day} ${hour}:${formattedMinute}`;
-}
-
-
+  }
 
   const tempComments = [
     {
@@ -90,76 +87,70 @@ function FeedPosts({ posts, postClickHandler }: FeedPostsProps) {
 
   return posts.map((post, index) => {
     return (
-        <div key={index}>
-          <div
-            
-            className="mt-4 p-3 border-[1px] rounded-lg border-black dark:border-0 shadow-2xl bg-white dark:bg-darkCoolGray"
-          >
-            <div className="flex justify-between border-gray-700 pb-2 border-b-[1px]">
-              <div className="flex">
-                <img
-                  className="h-12 w-auto rounded-full object-cover"
-                  src={post.pfp}
-                  alt="Your Company"
-                ></img>
+      <div key={index}>
+        <div className="mt-4 p-3 border-[1px] rounded-lg border-black dark:border-0 shadow-2xl bg-white dark:bg-darkCoolGray">
+          <div className="flex justify-between border-gray-700 pb-2 border-b-[1px]">
+            <div className="flex">
+              <img
+                className="h-12 w-auto rounded-full object-cover"
+                src={post.pfp}
+                alt="Your Company"
+              ></img>
+              <div>
+                <h2 className="text-md text-black font-semibold dark:text-white ml-1">
+                  {`${post.firstName} ${post.lastName}`}
+                </h2>
                 <div>
-                  <h2 className="text-md text-black font-semibold dark:text-white ml-1">
-                    {`${post.firstName} ${post.lastName}`}
-                  </h2>
-                  <div>
-                    <a
-                      href="#"
-                      className="text-gray-500 text-md ml-1 py-2"
-                      aria-current="page"
-                    >
-                      @{post.username}
-                    </a>
-                  </div>
+                  <a
+                    href="#"
+                    className="text-gray-500 text-md ml-1 py-2"
+                    aria-current="page"
+                  >
+                    @{post.username}
+                  </a>
                 </div>
               </div>
-              <div className="my-auto">
-                <button className=" font-semibold  text-neonBlue p-[1px] pl-2 pr-2 rounded-md">
-                  Follow
-                </button>
-              </div>
             </div>
-
-
-            <div className="mt-4">
-              <p className="dark:text-white text-black">{post.postBody}</p>
+            <div className="my-auto">
+              <button className=" font-semibold  text-neonBlue p-[1px] pl-2 pr-2 rounded-md">
+                Follow
+              </button>
             </div>
-            {post.postHashtags && (
-              <div className="mt-0">
-                {post.postHashtags.map((hashtag, hashIndex) => {
-                  return (
-                    <a className="mr-1 text-gray-500" key={hashIndex}>
-                      #{hashtag.hashtagText}
-                    </a>
-                  );
-                })}
-              </div>
-            )}
+          </div>
 
-            <div className="mt-4 border-gray-700 pb-2 border-b-[1px]">
-              <img
-                onClick={() => postClickHandler(posts[index])}
-                className="postContentImg w-[90%] border-[2px] border-gray-500 rounded-xl"
-                src={post.postPreview || "no preview"}
-                alt="Post"
-              ></img>
+          <div className="mt-4">
+            <p className="dark:text-white text-black">{post.postBody}</p>
+          </div>
+          {post.postHashtags && (
+            <div className="mt-0">
+              {post.postHashtags.map((hashtag, hashIndex) => {
+                return (
+                  <a className="mr-1 text-gray-500" key={hashIndex}>
+                    #{hashtag.hashtagText}
+                  </a>
+                );
+              })}
             </div>
+          )}
+
+          <div className="mt-4 border-gray-700 pb-2 border-b-[1px]">
+            <img
+              onClick={() => postClickHandler(posts[index])}
+              className="postContentImg w-[90%] border-[2px] border-gray-500 rounded-xl"
+              src={post.postPreview || "no preview"}
+              alt="Post"
+            ></img>
+          </div>
+          <div
+            className={`${
+              expandedPosts[index] ? "h-88 scale-100" : " h-0 scale-0"
+            }transition-all duration-200 ease-in-out `}
+          >
             <div
-              className={`${
-                expandedPosts[index] ? "h-88" : " h-0"
-              } w-full mt-2 transition-all duration-400 ease-in-out border-[2px] rounded-xl p-2 border-black`}
+              className={`${expandedPosts[index] ? "h-88 scale-100" : " h-0 scale-0"} ease-in transition-all duration-300 mt-2 border-[2px] rounded-xl p-2 border-black`}
             >
-              <div
-                className={`${
-                  expandedPosts[index] ? "block" : " hidden"
-                }`}
-              >
-                <form className="border-black pb-2 border-b-[2px]">
-                  <div className="flex">
+              <form className={`${expandedPosts[index] ? "scale-100" : "scale-0"} ease-in transition-all duration-200 border-black pb-2 border-b-[2px]`}>
+                <div className="flex">
                   <textarea
                     placeholder="Leave a comment, (200 characters max)"
                     className="bg-transparent border-solid border-customPurple text-black dark:text-white border-[1px] outline-none w-[75%] h-16 max-h-16"
@@ -167,100 +158,100 @@ function FeedPosts({ posts, postClickHandler }: FeedPostsProps) {
                   <button className="ml-2 mr-2 px-4 py-2 mt-2 border border-customPurple rounded-md h-10 self-end shadow-sm text-sm font-medium text-black dark:text-white bg-transparent hover:bg-indigo-700 transition duration-300 hover:scale-105">
                     Comment
                   </button>
-                  </div>
-                </form>
-                <div
-                  className={`max-h-64 overflow-y-scroll feedPostCommentSection`}
-                >
-                  {tempComments.map((comment, commentIndex) => (
-                    <div
-                      key={commentIndex}
-                      className="flex justify-between border-gray-700 pb-2 border-b-[1px]"
-                    >
-                      <p className="dark:text-white transition-all duration-500 ease-in-out text-black self-end max-w-[70%]">
-                        {comment.comment}
-                      </p>
-
-                      <a className="dark:text-white transition-all duration-500 ease-in-out text-black cursor-pointer self-end text-lg">
-                        -{comment.user}
-                      </a>
-                    </div>
-                  ))}
                 </div>
-              </div>
-            </div>
+              </form>
+              <div
+                className={`${expandedPosts[index] ? "scale-100 max-h-64" : "scale-0"} overflow-y-scroll feedPostCommentSection`}
+              >
+                {tempComments.map((comment, commentIndex) => (
+                  <div
+                    key={commentIndex}
+                    className="flex justify-between border-gray-700 pb-2 border-b-[1px]"
+                  >
+                    <p className="dark:text-white transition-all duration-500 ease-in-out text-black self-end max-w-[70%]">
+                      {comment.comment}
+                    </p>
 
-            <div className="flex justify-between">
-              <div className="flex mt-1">
-                <button
-                  type="button"
-                  className="rounded-full mr-3 p-1 text-customPurpleDark dark:text-white "
-                >
-                  <span className="sr-only">Like</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                    />
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className="rounded-full mr-3 p-1 text-customPurpleDark dark:text-white"
-                  id={`commentBtn${index}`}
-                  onClick={() => showCommentsFn(index)}
-                >
-                  <span className="sr-only">Comment</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-                    />
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className="rounded-full mr-3 p-1 text-customPurpleDark dark:text-white"
-                >
-                  <span className="sr-only">Share</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                    />
-                  </svg>
-                </button>
+                    <a className="dark:text-white transition-all duration-500 ease-in-out text-black cursor-pointer self-end text-lg">
+                      -{comment.user}
+                    </a>
+                  </div>
+                ))}
               </div>
-              <h2 className="mt-4 text-sm text-gray-500">
-              <p>Posted: {formatDate(post.postDate)}</p>
-              </h2>
             </div>
           </div>
+
+          <div className="flex justify-between">
+            <div className="flex mt-1">
+              <button
+                type="button"
+                className="rounded-full mr-3 p-1 text-customPurpleDark dark:text-white "
+              >
+                <span className="sr-only">Like</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                  />
+                </svg>
+              </button>
+              <button
+                type="button"
+                className="rounded-full mr-3 p-1 text-customPurpleDark dark:text-white"
+                id={`commentBtn${index}`}
+                onClick={() => showCommentsFn(index)}
+              >
+                <span className="sr-only">Comment</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
+                  />
+                </svg>
+              </button>
+              <button
+                type="button"
+                className="rounded-full mr-3 p-1 text-customPurpleDark dark:text-white"
+              >
+                <span className="sr-only">Share</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                  />
+                </svg>
+              </button>
+            </div>
+            <h2 className="mt-4 text-sm text-gray-500">
+              <p>Posted: {formatDate(post.postDate)}</p>
+            </h2>
+          </div>
         </div>
+      </div>
     );
   });
 }
