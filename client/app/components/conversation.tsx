54c6c337691092bbcbc9f48297b13d13413c982b
Carter <carreejoh4637@gmail.com>
2023-08-07 21:17:40 -0600

    Messages styling almost done, components for texts and conversation

--- client/app/components/conversation.tsx
+++ client/app/components/conversation.tsx
@@ -1,52 +1,79 @@
+import TheirText from "./theirTextComponent";
+import YourText from "./yourTextComponent";
+
 interface Convo {
   speaker: boolean;
   message: string;
 }
 
-function Conversations({ user, convo }: { user: String; convo: Convo[] }) {
+function Conversations({
+  pfp,
+  user,
+  convo,
+}: {
+  user: String;
+  convo: Convo[];
+  pfp: string;
+}) {
   const buildConversation = () => {
     if (user === "Nobody") {
-      return <h1>No user selected</h1>;
+      return (
+        <>
+        <div className="flex w-full h-16 p-2 justify-center border-b-2 bg-darkestWhite dark:bg-darkModeDarkGray border-mainDarkPurple items-center">
+            <div className="flex items-center h-full">
+              <div>
+                <a
+                  href="#"
+                  className="text-black dark:text-white font-semibold cursor-pointer text-xl mr-2 py-2"
+                  aria-current="page"
+                >
+                  Press someone to chat with!
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-[100%] p-2 overflow-y-scroll friendListMessages bg-white dark:bg-darkModeDarkestGray pb-32 h-[82vh]">
+            
+          </div>
+        </>
+      );
     }
     if (!convo[0]) {
-      return <h1>You have no messages with this user</h1>;
+      return (
+        <h1 className="text-black dark:text-white text-center">
+          You have no messages with this user
+        </h1>
+      );
     } else {
       return (
         <>
-          <h1 className="flex justify-center mb-8">{user}</h1>
-          <div className="w-[100%] mt-8 p-2">
+          <div className="flex w-full h-16 p-2 justify-center border-b-2 bg-darkestWhite dark:bg-darkModeDarkGray border-mainDarkPurple items-center">
+            <div className="flex items-center h-full">
+              <div>
+                <a
+                  href="#"
+                  className="text-black dark:text-white font-semibold cursor-pointer text-xl mr-2 py-2"
+                  aria-current="page"
+                >
+                  @{user}
+                </a>
+              </div>
+              <a className="cursor-pointer">
+                <img
+                  src={pfp}
+                  className="h-12 w-12 rounded-full object-cover border-customPurple border-2"
+                ></img>
+              </a>
+            </div>
+          </div>
+
+          <div className="w-[100%] p-2 overflow-y-scroll friendListMessages bg-white dark:bg-darkModeDarkestGray pb-32 h-[82vh]">
             {convo.map((text, index) => {
               if (text.speaker === true) {
-                return (
-                  <div key={index} className="flex justify-end mt-2">
-                    <div className="max-w-[60%]">
-                      <h1 className="border-2 border-blue-600 rounded-2xl p-1 bg-blue-300 text-black text-sm font-semibold">
-                        {text.message}
-                      </h1>
-                      <div className="flex justify-end mt-1">
-                        <h1 className="text-xs text-gray-500">
-                          8:01 PM, 08/01/2023
-                        </h1>
-                      </div>
-                    </div>
-                  </div>
-                );
+                return <YourText text={text.message} key={index} />;
               }
               if (text.speaker === false) {
-                return (
-                    <div key={index} className="flex justify-start mt-2">
-                    <div className="max-w-[60%]">
-                      <h1 className="border-2 border-green-600 rounded-2xl p-1 bg-green-300 text-black text-sm font-semibold">
-                        {text.message}
-                      </h1>
-                      <div className="flex justify-start mt-1">
-                        <h1 className="text-xs text-gray-500">
-                          8:01 PM, 08/01/2023
-                        </h1>
-                      </div>
-                    </div>
-                  </div>
-                );
+                return <TheirText text={text.message} key={index} />;
               }
             })}
           </div>
