62a8ae71afb08db74666c6e1237996d098a862b4
jonny desktop <jon@uslms.com>
2023-08-09 14:17:16 -0600

    defined path in the server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
