2caedfa28fc85438debec97a80a5d530794f92ec
Carter <carreejoh4637@gmail.com>
2023-08-09 01:27:09 -0600

    Whole new profile section

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,16 +1,16 @@
-import Sidebar from "./components/sidebar"
+import Sidebar from "./components/sidebar";
 
-export const dynamic = 'auto',
-dynamicParams = true,
-revalidate = Infinity,
-fetchCache = 'auto',
-runtime = 'nodejs',
-preferredRegion = 'auto'
+export const dynamic = "auto",
+  dynamicParams = true,
+  revalidate = Infinity,
+  fetchCache = "auto",
+  runtime = "nodejs",
+  preferredRegion = "auto";
 
 export default function Loading() {
-    return(
-        <div className="bg-gradient-to-t  from-mainPurple to-mainDarkPurple dark:from-mainDarkPurple dark:to-mainPurple h-[100vh] fixed top-0 left-0 w-20 shadow-lg z-[200]">
-            
-        </div>
-    )
+  return (
+    <div className="w-[100%]">
+        
+    </div>
+  );
 }
