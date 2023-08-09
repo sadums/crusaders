e4bee9479c9ed8e1c68d7eccefb949b425c0c790
Carter <carreejoh4637@gmail.com>
2023-08-09 02:44:02 -0600

    New profile section more than halfway

--- client/app/components/profile/profileSideInfo.tsx
+++ client/app/components/profile/profileSideInfo.tsx
@@ -25,7 +25,7 @@ function ProfileSideInfo({ userInfo }: any) {
         </p>
         <div className="relative flex items-start space-x-4 mt-2 border-b-[1px] border-gray-700 dark:border-gray-500 pb-2">
           {/* ADD THE REAL FUCTIONALIY OF THESE ASK SAM ABOUT IT */}
-          <button className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-mainBlue hover:bg-mainDarkBlue duration-300 ">
+          {/* <button className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-mainBlue hover:bg-mainDarkBlue duration-300 ">
             Follow
           </button>
           <button
@@ -78,7 +78,7 @@ function ProfileSideInfo({ userInfo }: any) {
             </>
           )}
         </div>
-      </div> */}
+      </div> */} 
       {Auth.loggedIn() && (
         <>
           {Auth.getProfile().data._id === userData._id && (
