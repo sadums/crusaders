373fe7efb795f8d129003aebc7f1197cf35f6690
sadums <samjadams4@gmail.com>
2023-08-09 21:35:08 -0600

    added create message mutation

--- client/app/Messages/[chatId]/page.tsx
+++ client/app/Messages/[chatId]/page.tsx
@@ -1,19 +1,46 @@
 "use client"
 
-import { useQuery, useSubscription } from "@apollo/client"
+import { useMutation, useQuery, useSubscription } from "@apollo/client"
 import { GET_CHAT_BY_ID } from "../../GraphQL/queries"
 import { CHAT_SUBSCRIPTION } from "../../GraphQL/subscriptions"
+import { CREATE_MESSAGE } from "@/app/GraphQL/mutations"
 import Auth from "../../(utils)/auth";
 
 export default function Page({ params }: { params: { chatId: string } }) {
-  console.log(Auth.getProfile().data._id) // userId
+  const userId = Auth.getProfile().data._id
+
+  
+  // GET INITIAL CONVERSATION DATA
+  const { data:chatData, loading:chatLoading, error:chatError } = useQuery(GET_CHAT_BY_ID, {
+    variables: {
+      chatId: params.chatId
+    }
+  });
+
+  // WEB SOCKET TO GET NEW INFORMATION
   const { data, loading, error } = useSubscription(CHAT_SUBSCRIPTION, {
     variables: {
       chatId: params.chatId,
-      userId: Auth.getProfile().data._id,
+      userId: userId,
     },
   });
+  console.log(chatData);
+  console.log(data);
+
+
+
+
+  // CREATE MESSAGE EXAMPLE
+  const [createMessage, {data:messageData, loading:messageLoading, error:messageError}] = useMutation(CREATE_MESSAGE);
+
+  createMessage({
+    variables:{
+      userId: userId,
+      chatId: params.chatId,
+      body: "" // this is what the user will send as their message
+    }
+  });
   
-  console.log(data)
+
   return (<div>My Post: {params.chatId}</div>)
 }
